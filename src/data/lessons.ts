b1f414f6356b97dfb1914adf7b170619c13801b9
Wissem Redjem <wissem.redjem.dev@gmail.com>
2025-06-17 16:19:58 +0100

    Update greetings lesson

--- src/data/lessons.ts
+++ src/data/lessons.ts
@@ -6,8 +6,28 @@ export const lessons: Lesson[] = [
     title: "Greetings",
     vocab: [
       { kabyle: "Azul", english: "Hello" },
-      { kabyle: "Amek iḥul-ik?", english: "How are you?" }
+      { kabyle: "Sbaḥ-lxir", english: "Good morning" },
+      { kabyle: "Ɛelxir", english: "Good morning" },
+      { kabyle: "Axir", english: "Good morning" },
+      { kabyle: "Mselxir", english: "Good evening" },
+      { kabyle: "Iḍ ameggaz", english: "Good night" },
+      { kabyle: "Iḍ yelhan", english: "Good night" },
+      { kabyle: "Mreḥba", english: "Welcome" },
+      { kabyle: "Lɛeslama", english: "Welcome" },
+      { kabyle: "Ar tufat", english: "Goodbye" },
+      { kabyle: "Ar timlilit", english: "Goodbye" },
+      { kabyle: "Ass igerrzen", english: "Have a good day" },
+      { kabyle: "Tamussni n lxir", english: "Nice to meet you" },
+      { kabyle: "Tanemmirt", english: "Thank you" },
+      { kabyle: "Saḥit", english: "Thank you" },
+      { kabyle: "Suref-iyi", english: "I'm sorry/Excuse me" },
+      { kabyle: "Semḥiyi", english: "I'm sorry/Excuse me" },
+      { kabyle: "Amek tettiliḍ", english: "How are you?" },
+      { kabyle: "Afud igerrzen", english: "Good luck" },
+      { kabyle: "S tezmert-ik(im)", english: "Bon Appetit" },
+      { kabyle: "Aseggas ameggaz", english: "Happy new year" },
+      { kabyle: "Ttxilek", english: "Please" },
     ],
-    examples: ["Azul fell-awen! – Hello to all of you!"]
+    examples: ["Azul fell-awen! - Hello to all of you!"]
   }
 ]
